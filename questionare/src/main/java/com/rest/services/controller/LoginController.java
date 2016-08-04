e8750cae84731a7c138f08f7db21d71b0fa6969e
sandeepkoyyada <sandeep088@gmail.com>
2016-08-03 23:39:43 -0500

    link with mysql connections and dao folders

--- questionare/src/main/java/com/rest/services/controller/LoginController.java
+++ questionare/src/main/java/com/rest/services/controller/LoginController.java
@@ -1,20 +1,23 @@
 package com.rest.services.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rest.services.manager.UserDetailsManager;
 import com.rest.services.util.RestObject;
 
 @RestController
 public class LoginController {
 
+	@Autowired
+	UserDetailsManager userDetailsManager;
+	
 	@RequestMapping(value = "/HelloWord", method = RequestMethod.GET, headers = "Accept=application/json")
 	public RestObject getHelloWorld() {
 		String message = "Hello World";
@@ -37,4 +40,19 @@ public class LoginController {
 		//RestObject response = new RestObject(message, 200, "Success");
 		return restObject;
 	}
+	
+	
+	@RequestMapping(value = "/getUser/{userName}", method = RequestMethod.GET, headers = "Accept=application/json")
+	public @ResponseBody 
+	RestObject getUser(@PathVariable String userName) {
+		
+		String full_name = this.userDetailsManager.getUserName(userName);
+		
+		RestObject restObject = new RestObject(full_name,200,"Success");
+		
+		//RestObject response = new RestObject(message, 200, "Success");
+		return restObject;
+	}
+	
+	
 }
